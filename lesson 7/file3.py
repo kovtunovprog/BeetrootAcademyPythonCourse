f375e1197970cef3082c0bdd33010d12b414f3d3
ArturYastrebov <AOYastrebov@gmail.com>
2022-07-14 07:43:34 +0300

    lesson7 Artur_Yastrebov

--- lesson 7/file3.py
+++
@@ -1,2 +0,0 @@
-mylist = [(i, i**2) for i in range(1, 11)]
-print(mylist)
--- lesson 7/file4.py
+++
@@ -1,5 +0,0 @@
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-my_dict_1 = {key:value for key,value in enumerate(days, start=1)}
-my_dict_2 = {value:key for key,value in enumerate(days, start=1)}
-print(my_dict_1)
-print(my_dict_2)
