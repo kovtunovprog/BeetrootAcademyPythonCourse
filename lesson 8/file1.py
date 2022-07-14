99c9ca5a31906da7c2f682b87a7df4d3f6530340
ArturYastrebov <AOYastrebov@gmail.com>
2022-07-14 08:03:17 +0300

    lesson8 Artur_Yastrebov

---
+++ lesson 8/file1.py
@@ -0,0 +1,4 @@
+def favorite_movie(name):
+    print(f'My favorite movie is named {name}.')
+
+favorite_movie('Home Alone')
---
+++ lesson 8/file2.py
@@ -0,0 +1,5 @@
+def make_country(country,capital):
+    mydict = {country:capital}
+    print(mydict)
+
+make_country('Austria','Vienna')
