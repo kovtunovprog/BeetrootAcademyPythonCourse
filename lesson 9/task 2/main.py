7023fa50e74bcb3b396717ae37f1d3002d28988d
Irina Cherkaska <amadare55555@gmail.com>
2022-07-23 02:28:32 +0300

    Irina Cherkaska Lesson 9

--- lesson 9/task 2/main.py
+++ lesson 9/task 2/main.py
@@ -1,7 +1,7 @@
 #  The sys module.
 #  The “sys.path” list is initialized from the PYTHONPATH environment variable. 
 #  Is it possible to change it from within Python? If so, does it affect where Python looks for module files? 
-#  Run some interactive tests to find it out.
+#  Run some interactive tests to find it out
 
 import sys
 print(sys.path)
--- lesson 9/task3/main.py
+++
@@ -1,7 +0,0 @@
-# Task 3
-# Basics, import, work with os module
-import mymod
-mymod.test('./lesson 9/task3/txt.txt')
-mymod.test('./lesson 9/task3/2.txt')
-
-
