015bf864a27518aa8e39c59b1b83399cd2276076
Oleksandr Kovtunov <kovtunov.prog@gmail.com>
2022-07-23 11:24:30 +0300

    Lesson 11

---
+++ cource/lesson_11/file.py
@@ -0,0 +1,20 @@
+# f = open("file.txt", "w")
+# print(f.write("HELLO WORLD"))
+# f.close()
+
+# with open("file.txt", "a") as f:
+#     f.write("\nfooo2")
+import json
+
+# with open("file.json", "r") as f:
+#     print(f.read()[:20])
+
+# with open("file.json", "r") as f:
+#     data = json.load(f)
+#     data["users"].append(2)
+#     print(data)
+
+# data = {"users": [1, 2, 3]}
+#
+# with open("file.json", "w") as f:
+#     json.dump(data, f)
