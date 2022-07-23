2b93589892b143a300e0765168a58ffd44d291e2
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-07-23 13:17:33 +0300

    Update parse_users_from_json_file and save_users_to_json_file

--- cource/lesson_12/utils.py
+++ cource/lesson_12/utils.py
@@ -1,6 +1,9 @@
 def parse_users_form_json_file(filename: str) -> list:
-    raise NotImplementedError
+    with open(filename, 'r') as file_object:
+        f = json.load(file_object)
+    return(f)
 
 
 def save_users_to_json_file(filename: str, data: list) -> str:
-    raise NotImplementedError
+    with open(filename, 'w') as data:
+        json.dump(filename, data)
