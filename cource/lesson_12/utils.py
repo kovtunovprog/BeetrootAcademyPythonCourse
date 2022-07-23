934178ae853883016613ad9f41f9fd610d144291
dpirohov <1355579555AsdF>
2022-07-23 13:16:03 +0300

    updated parse_users_from_json

--- cource/lesson_12/utils.py
+++ cource/lesson_12/utils.py
@@ -1,6 +1,14 @@
 def parse_users_form_json_file(filename: str) -> list:
-    # TODO: if [[]] - > error crash
-    raise NotImplementedError
+    with open(filename, 'r')
+        try:
+            users = json.load(f)['users']
+            for i in users:
+                if len(i) != 6:
+                    raise ValueError('Seems like some keys are missing')
+        except TypeError:
+            raise TypeError('Seems like your data type is incorrect (Expecting: [{}])')
+
+    return users
 
 
 def save_users_to_json_file(filename: str, data: list) -> str:
