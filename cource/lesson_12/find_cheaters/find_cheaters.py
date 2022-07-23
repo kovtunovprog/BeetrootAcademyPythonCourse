1bce570ff68f4a306f29ddbd1e2be65aaa3b8f14
Grau01 <oleksandr.deineka@beetroot.se>
2022-07-23 13:20:17 +0300

    Deineka changed

--- cource/lesson_12/find_cheaters/find_cheaters.py
+++ cource/lesson_12/find_cheaters/find_cheaters.py
@@ -1,8 +1,8 @@
 def find_cheaters(users: list) -> list:
     for i in users:
-        balance_max = (i['level'] + i['kills']) * 1000 - i['spent']
+        balance_max = (int(i['level']) + int(i['kills'])) * 1000 - int(i['spent'])
         try:
-            if i['balance'] != balance_max:
+            if int(i['balance']) != balance_max:
                 raise NotImplementedError as err
         except NotImplementedError:
             print(err)
