b75b06769f742e4fd522a13b09c91102e1b5e46b
Alaru-dev <pobrom@gmail.com>
2022-07-23 13:19:55 +0300

    update #1 functions in find_cheaters

--- cource/lesson_12/find_cheaters/find_cheaters.py
+++ cource/lesson_12/find_cheaters/find_cheaters.py
@@ -1,48 +1,49 @@
-def find_cheaters(users: list) -> list:
-    raise NotImplementedError
+# def find_cheaters(users: list) -> list:
+#     raise NotImplementedError
 
 
 
 # check input data type and transform in need type
-def change_type_analise(users) -> tuple:
+from poberezhni_roman.lesson_10.cw_10.p import users_2
+
+
+def change_type_analise(users) -> list:
     try:
         if type(users[0]) == dict:
             return change_dicts_in_tuple_and_check_data_correct(users)
-        elif type(users[0]) == tuple or type(users[0]) == list:
-            return users
+        elif type(users[0]) == list:
+            return is_data_correct_list(users)
     except TypeError:
         print(TypeError, "Incorrect data type!")
 
 
-def change_dicts_in_tuple_and_check_data_correct(users) -> tuple:
-    l = [list(users[0].keys())]
-    for user in list(users):
+def change_dicts_in_tuple_and_check_data_correct(users) -> list:
+    users_new = [users[0].keys()]
+    for user in users:
         # check is user data correct and print result if not
         try:
-            if len(user.keys()) != len(l[0]):
+            if len(user.keys()) != len(users_new[0]):
                 raise ValueError(f'Invalid user data: {user}. It not include in analise.\n')
         except ValueError as v_er:
             print(ValueError, v_er)
             continue
         # create data from dict values for user and add in users_new
-        l.append(change_user_dct_in_tpl(user))
-    users_new = tuple(l)
+        users_new.append(change_user_dict_in_tupl(user))
     return users_new
 
 
-def change_user_dct_in_tpl(user: dict) -> list:
+def change_user_dict_in_tupl(user: dict) -> list:
     user_data = [*(i for i in user.values())]
     return user_data
 
 
 # check is users data correct (maybe miss some element) and build new list with lists of user data
-def is_data_correct_tuple(users: tuple) -> list:
-    userss = list(change_type_analise(users))
+def is_data_correct_list(users: list) -> list:
     users_new = []
-    for user in userss:
+    for user in users:
         user = list(user)
         try:
-            if len(user) != len(userss[0]):
+            if len(user) != len(users[0]):
                 raise ValueError(f'Invalid user data: {user}. It not include in analise.\n')
         except ValueError as v_er:
             print(ValueError, v_er)
@@ -52,51 +53,46 @@ def is_data_correct_tuple(users: tuple) -> list:
 
 
 # find all user data as int in variables
-def find_user_data_int(users: tuple, user) -> vars:
-    userss = is_data_correct_tuple(users)
-    # user1 = change_user_dct_in_tpl(user)
+def find_user_data_int(users: list, user) -> vars:
+    userss = change_type_analise(users)
     level = int(user[userss[0].index('level')])
     kills = int(user[userss[0].index('kills')])
     balance = int(user[userss[0].index('balance')])
-    if 'spent' in userss[0]:
-        spent = int(user[userss[0].index('spent')])
-        return level, kills, balance, spent
-    else:
-        return level, kills, balance
-
+    spent = int(user[userss[0].index('spent')])
+    return level, kills, balance, spent
 
 
 # check user balance by criteria make output True/False and true_balance
-def check_is_user_cheater(users: tuple, user) -> [bool, int]:
+def check_is_user_cheater(users: list, user) -> bool:
+    index = find_user_data_int(users, user)
+    balance = index[2]
+    correct_balance = get_correct_user_balance(users, user)
+    return (balance != correct_balance)
+
+def get_correct_user_balance(users, user) -> int:
     index = find_user_data_int(users, user)
     level = index[0]
     kills = index[1]
     balance = index[2]
+    spent = index[3]
     max_balance = level * 1000 + kills * 1000
-    if len(index) == 4:
-        spent = index[3]
-        if (max_balance - spent) >= 0:
-            true_balance = (max_balance - spent)
-        else:
-            true_balance = 0
-        return ((balance + spent) > max_balance), true_balance
-    else:
-        return (balance > max_balance), max_balance
+    correct_balance = max_balance - spent
+    return correct_balance
 
-def find_cheaters_list(users_new: tuple) -> (tuple, list):
-    users_new = is_data_correct_tuple(users_new)
+
+def find_cheaters(users_new: list) -> list:
+    users_new = change_type_analise(users_new)
     cheaters_l = []
-    # cheaters_l.append(tuple(users_new[0]))
     for user in users_new[1:]:
         try:
-            if check_is_user_cheater(users_new, user)[0]:
+            if check_is_user_cheater(users_new, user):
                 raise ValueError(f"Found  cheater: {user[0:2]}")
         except ValueError as v_er:
             print(ValueError, v_er)
-            cheaters_l.append(tuple(user))
+            cheaters_l.append(user)
 
-    return tuple(cheaters_l)
+    return cheaters_l
 
 if __name__ == '__main__':
-    from poberezhni_roman.lesson_10.cw_10.p import users_1
-    print(find_cheaters_list(users_1))
+
+    print(find_cheaters(users_2))
